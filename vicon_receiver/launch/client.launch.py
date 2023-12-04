c7ddfad1acdc463e8066ca93f0776cad94071ba0
Federico Ciresola <cirefede@gmail.com>
2023-12-04 15:22:45 +0100

    fix: replaced paramtername on LaunchDescription I changed "node_executable" into "executable" because the first has been deprecated

--- vicon_receiver/launch/client.launch.py
+++ vicon_receiver/launch/client.launch.py
@@ -8,6 +8,6 @@ def generate_launch_description():
     topic_namespace = 'vicon'
 
     return LaunchDescription([Node(
-            package='vicon_receiver', node_executable='vicon_client', output='screen',
+            package='vicon_receiver', executable='vicon_client', output='screen',
             parameters=[{'hostname': hostname, 'buffer_size': buffer_size, 'namespace': topic_namespace}]
         )])
