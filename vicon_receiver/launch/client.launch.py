f2d05a8c479e6682d1057bc8c20a0b7a0b5dec9e
Federico Ciresola <cirefede@gmail.com>
2024-05-31 14:32:30 +0200

    feat: added parameter calle ip_vicon_pc

--- vicon_receiver/launch/client.launch.py
+++ vicon_receiver/launch/client.launch.py
@@ -1,13 +1,22 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
 
-    hostname = '192.168.10.1'
     buffer_size = 200
     topic_namespace = 'vicon'
 
-    return LaunchDescription([Node(
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'ip_vicon_pc', default_value='192.168.10.1'
+        ),
+        Node(
             package='vicon_receiver', executable='vicon_client', output='screen',
-            parameters=[{'hostname': hostname, 'buffer_size': buffer_size, 'namespace': topic_namespace}]
-        )])
+            parameters=[{
+                'hostname': LaunchConfiguration('ip_vicon_pc'), 
+                'buffer_size': buffer_size, 
+                'namespace': topic_namespace
+            }])
+    ])
